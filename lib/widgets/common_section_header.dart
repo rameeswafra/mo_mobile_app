53bdf6198c591c4d6428b8dde683b54193456a40
wafra_r <wafra_r@epiclanka.net>
2025-12-14 10:00:08 +0530

    common header

---
+++ lib/widgets/common_section_header.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CommonSectionHeader extends StatelessWidget {
+  final String title;
+
+  const CommonSectionHeader({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
