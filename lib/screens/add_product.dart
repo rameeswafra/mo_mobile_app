413de0c7d1082f22d5281bf77c08b36bdb3697ef
wafra_r <wafra_r@epiclanka.net>
2025-12-19 20:18:17 +0530

    Add Products Screen

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:mo_app/widgets/common_app_bar.dart';
+import '../widgets/common_app_bar.dart';
 
-class AddProductScreen extends StatefulWidget {
+class AddProductScreen extends StatelessWidget {
   const AddProductScreen({super.key});
 
   @override
-  State<AddProductScreen> createState() => _AddProductScreenState();
-}
-
-class _AddProductScreenState extends State<AddProductScreen> {
-  @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final bgColor = isDark ? Colors.grey[900] : Colors.grey[100];
+
     return Scaffold(
-      backgroundColor: Colors.grey.shade100,
-      appBar:CommonAppBar(isHomePage: false,
+      backgroundColor: bgColor,
+      appBar: const CommonAppBar(
+        isHomePage: false,
         isProductListPage: true,
-      title: "Add Products",
-      ) ,
+        title: "Add Products",
+      ),
+      body: const Center(
+        child: Text(
+          "Add Products Screen",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
     );
   }
 }
